be757ab53facc57880ec63a02853c8a360b2d69e
Radzivon-Kliuchnikau <rodionk510@gmail.com>
2024-12-19 12:28:20 +0100

    0016 Auth-Second: Add styles to footer, add dropdown to navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import {Box, Typography} from "@mui/material";
 
 const Footer = () => {
     return (
-        <Box>
+        <Box sx={{display: "flex", justifyContent: "center", padding: "20px"}}>
             <Typography>{new Date().getFullYear()} RareCase, Inc.</Typography>        
         </Box>
     );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,32 @@
-import {AppBar, Avatar, Box, Button, styled, Toolbar, Typography} from "@mui/material";
-import {Checklist, Person} from "@mui/icons-material";
+import {
+    AppBar,
+    Avatar,
+    Box,
+    Button,
+    IconButton, ListItemIcon, Menu,
+    MenuItem,
+    styled,
+    Toolbar,
+    Tooltip,
+    Typography
+} from "@mui/material";
+import {Checklist, Logout, Person} from "@mui/icons-material";
 import useAuth from "../hooks/UseAuth.tsx";
 import {Link} from "react-router-dom";
+import {useState} from "react";
 
 const LogoImage = styled("img")(({theme}) => ({}))
 
 const Navbar = () => {
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     const {authUser} = useAuth();
     const StyledToolbar = styled(Toolbar)({
@@ -66,7 +87,64 @@ const Navbar = () => {
                     <Box sx={{display: "flex", alignItems: "center", gap: "10px"}}>
                         <Typography sx={{color: "black"}}>
                             Hey, {authUser.Name}</Typography>
-                        <Avatar src="/static/avatar.jpg"/>
+                        <Tooltip title="Account settings">
+                            <IconButton
+                                onClick={handleClick}
+                                size="small"
+                                sx={{ml: 2}}
+                                aria-controls={open ? 'account-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                            >
+                                <Avatar sx={{width: 32, height: 32}}><Person/></Avatar>
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            anchorEl={anchorEl}
+                            id="account-menu"
+                            open={open}
+                            onClose={handleClose}
+                            onClick={handleClose}
+                            slotProps={{
+                                paper: {
+                                    elevation: 0,
+                                    sx: {
+                                        overflow: 'visible',
+                                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                        mt: 1.5,
+                                        '& .MuiAvatar-root': {
+                                            width: 32,
+                                            height: 32,
+                                            ml: -1.5,
+                                            mr: 1,
+                                        },
+                                        '&::before': {
+                                            content: '""',
+                                            display: 'block',
+                                            position: 'absolute',
+                                            top: 0,
+                                            right: 14,
+                                            width: 10,
+                                            height: 10,
+                                            bgcolor: 'background.paper',
+                                            transform: 'translateY(-50%) rotate(45deg)',
+                                            zIndex: 0,
+                                        },
+                                    },
+                                },
+                            }}
+                            sx={{
+                                top: `25px`,
+                                left: `${anchorEl?.getBoundingClientRect()?.right - 135}px`,
+                                transform: 'none'
+                            }}>
+                            <MenuItem onClick={handleClose}>
+                                <ListItemIcon>
+                                    <Logout fontSize="small"/>
+                                </ListItemIcon>
+                                Logout
+                            </MenuItem>
+                        </Menu>
                     </Box>
                 )}
             </StyledToolbar>
