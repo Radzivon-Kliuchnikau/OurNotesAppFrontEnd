04c0cdad73708ae058f7f59ef1012499a65ef6b3
Radzivon-Kliuchnikau <rodionk510@gmail.com>
2024-12-20 18:08:12 +0100

    #9: add new styles and content to main page

--- src/pages/OpenPage.tsx
+++ src/pages/OpenPage.tsx
@@ -6,34 +6,49 @@ function OpenPage() {
     return (
         <MainContainer>
             <Box
-                bgcolor="#f1e8b8"
                 flex={12}
                 p={2}
                 textAlign="center"
                 sx={{
                     display: "flex",
                     flexDirection: "column",
-                    justifyContent: "center",
+                    alignItems: "center",
+                    paddingTop: "100px"
                 }}
             >
-                <Typography component="p">
-                    Here we can create
-                </Typography>
-                <Typography sx={{fontSize:25}} component="p">
-                    Our Notes
+                <Box sx={{maxWidth: "800px", marginBottom: "40px"}}>
+                    <Typography
+                        variant="h1"
+                        sx={{
+                            fontSize: "4rem",
+                            fontWeight: 600
+                        }}
+                    >
+                        Create your notes, save it and share
+                    </Typography>
+                </Box>
+                <Typography
+                    component="h1"
+                    variant="h6"
+                    sx={{marginBottom: "40px"}}
+                >
+                    A little tool that help you and your partner to remember things
                 </Typography>
                 <Button
-                    variant="contained"
                     component={Link}
                     to="/registration"
+                    disableRipple
                     sx={{
-                        width: "221px",
-                        alignSelf: "center",
-                        margin: "30px"
+                        width: "120px",
+                        height: "30px",
+                        border: "1px solid black",
+                        borderRadius: "10px",
+                        color: "black",
+                        textDecoration: "none",
+                        textTransform: "none",
+                        fontSize: "16px"
                     }}
-                >
-                    Registration
-                </Button>
+                >Get started</Button>
             </Box>
         </MainContainer>
     );
