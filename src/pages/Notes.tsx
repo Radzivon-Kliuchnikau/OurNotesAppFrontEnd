4e32418ff40c2c464580db04d8f14622ec4e8880
Radzivon-Kliuchnikau <rodionk510@gmail.com>
2025-01-17 22:07:29 +0100

    #41: add edit functionality for notes page in a modal version

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -4,9 +4,8 @@ import {
     Card,
     CardActions,
     CardContent,
-    CircularProgress,
     Dialog, DialogActions, DialogContent, DialogTitle,
-    IconButton,
+    IconButton, TextField,
     Typography
 } from "@mui/material";
 import axios from "../api/axios.tsx";
@@ -24,26 +23,79 @@ const Notes = () => {
 
     const [errorMessage, setErrorMessage] = useState("");
     const [error, setError] = useState(false);
-    const [data, setData] = useState([]);
+    const [data, setData] = useState<NoteObject[] | []>([]);
+    const [noteTitle, setNoteTitle] = useState<string>("");
+    const [noteContent, setNoteContent] = useState<string>("");
     const [loading, setLoading] = useState(false);
-    const [openModal, setOpenModal] = useState(false);
+    const [openRemoveModal, setOpenRemoveModal] = useState(false);
+    const [openEditModal, setOpenEditModal] = useState(false);
     const [selectedNoteId, setSelectedNoteId] = useState<string | null>(null);
+    const [selectedNote, setSelectedNote] = useState<NoteObject | null>(null);
 
-    const handleOpenModal = (id: string) => {
-        setSelectedNoteId(id);
-        setOpenModal(true);
+    const handleOpenEditModal = (note: NoteObject) => {
+        if (openRemoveModal) return;
+        setSelectedNote(note);
+        setNoteTitle(note.title);
+        setNoteContent(note.content);
+        setOpenEditModal(true);
     }
 
-    const handleCloseModel = () => {
-        setOpenModal(false);
+    const handleCloseEditModal = () => {
+        setOpenEditModal(false);
+        setSelectedNote(null);
+        setNoteTitle("");
+        setNoteContent("");
+    }
+
+    const handleEditNote = async () => {
+        if (!selectedNote) return;
+
+        try {
+            const response = await axios.put(
+                `${API_URL.NOTES_URL}/${selectedNote.id}`,
+                JSON.stringify({title: noteTitle, content: noteContent}),
+                {
+                    headers: {"Content-Type": "application/json"},
+                }
+            )
+            const updatedData: NoteObject[] = data.map((item: NoteObject) =>
+                item.id === selectedNote.id ? {
+                    ...item,
+                    title: noteTitle,
+                    content: noteContent
+                } : item)
+            setData(updatedData);
+        } catch (error: any) {
+            if (!error.response) {
+                setErrorMessage("No Server Response")
+            } else if (error.response?.status === 401) {
+                setErrorMessage("Unauthorized")
+            } else {
+                setErrorMessage("Can't edit notes, we're working on it");
+            }
+
+            setError(true);
+        } finally {
+            handleCloseEditModal();
+        }
+    }
+
+    const handleOpenRemoveModal = (id: string) => {
+        setSelectedNoteId(id);
+        setOpenRemoveModal(true);
+    }
+
+    const handleCloseRemoveModel = () => {
+        setOpenRemoveModal(false);
         setSelectedNoteId(null);
     }
-    const handleDelete = async () => {
+
+    const handleNoteDelete = async () => {
         if (!selectedNoteId) return;
 
         try {
             const response = await axios.delete(
-                `${API_URL.DELETE_NOTE}/${selectedNoteId}`,
+                `${API_URL.NOTES_URL}/${selectedNoteId}`,
                 {
                     headers: {"Content-Type": "application/json"},
                     withCredentials: true
@@ -62,7 +114,7 @@ const Notes = () => {
 
             setError(true);
         } finally {
-            handleCloseModel();
+            handleCloseRemoveModel();
         }
     }
 
@@ -91,7 +143,6 @@ const Notes = () => {
         } finally {
             setLoading(false);
         }
-
     }
 
     useEffect(() => {
@@ -100,14 +151,38 @@ const Notes = () => {
 
     return (
         <MainContainer>
-            <Dialog open={openModal} onClose={handleCloseModel}>
+            <Dialog open={openRemoveModal} onClose={handleCloseRemoveModel}>
                 <DialogTitle>Are you sure?</DialogTitle>
                 <DialogContent>
                     <Typography>Do you actually want to remove this note?</Typography>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleCloseModel}>Cancel</Button>
-                    <Button onClick={handleDelete} color="error">Delete</Button>
+                    <Button onClick={handleCloseRemoveModel}>Cancel</Button>
+                    <Button onClick={handleNoteDelete} color="error">Delete</Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog open={openEditModal} onClose={handleCloseEditModal}>
+                <DialogTitle>Edit your note</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        value={noteTitle}
+                        onChange={(e) => setNoteTitle(e.target.value)}
+                        label="Title"
+                        fullWidth
+                    />
+
+                    <TextField
+                        value={noteContent}
+                        onChange={(e) => setNoteContent(e.target.value)}
+                        label="Note content"
+                        fullWidth
+                        multiline
+                        rows={4}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseEditModal}>Cancel</Button>
+                    <Button onClick={() => handleEditNote(selectedNote)}>Save</Button>
                 </DialogActions>
             </Dialog>
             {
@@ -151,12 +226,14 @@ const Notes = () => {
                             {data.map((note: NoteObject, index) => (
                                 <Card
                                     key={index}
+                                    onClick={() => handleOpenEditModal(note)}
                                     sx={{
                                         width: "300px",
                                         height: "150px",
                                         display: "flex",
                                         flexDirection: "column",
-                                        justifyContent: "space-between"
+                                        justifyContent: "space-between",
+                                        cursor: "pointer"
                                     }}>
                                     <CardContent sx={{margin: 0, padding: "10px 10px 0 10px"}}>
                                         <Typography component="h1" variant="h6">
@@ -173,7 +250,12 @@ const Notes = () => {
                                         </Typography>
                                     </CardContent>
                                     <CardActions sx={{justifyContent: "flex-end"}}>
-                                        <IconButton onClick={() => handleOpenModal(note.id)} aria-label="delete">
+                                        <IconButton
+                                            onClick={(e) => {
+                                                e.stopPropagation();
+                                                handleOpenRemoveModal(note.id)
+                                            }} 
+                                            aria-label="delete">
                                             <Delete/>
                                         </IconButton>
                                     </CardActions>
